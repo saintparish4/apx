cc353f73febb7140ff15fcfccaf21c7bd731fb90
Your Name <Your Email>
2025-12-15 10:11:09 -0500

    Test

---
+++ Core/src/test.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Test is Test {
+    function setUp() public {
+        // code
+    }
+
+    function test_Test() public {
+        // code
+    }
+}
